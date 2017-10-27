9291cf570ffc7045a3ae4a2d623b849dc69995d9
rodolfo.rocha <rodolfo.rocha@datametrica.com.br>
2017-10-27 11:39:50 -0300

    corrigindo metodo de receive client no teste

---
+++ Sockets_Teste/Client_Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+class Client_Test
+{
+    public static void Main(string[] args)
+    {
+        Client c1 = new Client(2004, "127.0.0.1");
+        c1.StartClient();
+        while (true)
+        {
+            string cmd = Console.ReadLine();
+
+            if (cmd == "sair")
+            {
+                c1.ReleaseSocket();
+            }
+            else 
+            {                
+                Console.WriteLine(cmd);
+                c1.Send(cmd);
+                c1.EndMessage();
+                c1.Receive(c1.GetSocket());
+                c1.GetSocketReceiveResponse();             
+                
+            }
+        }
+
+
+    }
+}
